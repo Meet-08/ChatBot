f3fff887fceb45c781ca13ee05f366d5c3acb142
Meet-08 <meetjbhuva@gmail.com>
2025-03-09 11:51:44 +0530

    Delete frontend directory

--- frontend/src/pages/page.js
+++
@@ -1,7 +0,0 @@
-import Chat from './Chat'
-import Home from './Home'
-import Login from './Login'
-import NotFound from './NotFound'
-import Signup from './Signup'
-
-export { Chat, Home, Login, NotFound, Signup }
