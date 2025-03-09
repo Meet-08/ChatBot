3954124c1531992dce0dd5ad962cc5a6e75e6022
Meet-08 <meetjbhuva@gmail.com>
2025-03-09 11:49:57 +0530

    Backend for deployment

--- backend/src/main/java/com/meet/aibot/security/Cors.java
+++ backend/src/main/java/com/meet/aibot/security/Cors.java
@@ -1,11 +1,17 @@
 package com.meet.aibot.security;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Configuration
 public class Cors {
 
